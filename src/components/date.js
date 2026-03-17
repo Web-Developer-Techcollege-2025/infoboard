2fcc06e830fef26ab19f2a3facffb9c9c3086c5a
Jens L. Hammer <jens@lindberghammer.dk>
2026-03-17 18:26:51 +0100

    Fixed casing of some files

--- src/components/date.js
+++
@@ -1,9 +0,0 @@
-export function date(date = new Date()) {
-    const currentDate = date.toLocaleDateString('da-DK', {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'short',
-        year: 'numeric'
-    });
-    return currentDate;
-}
--- src/components/time.js
+++
@@ -1,8 +0,0 @@
-export function time(date = new Date()) {
-    const currentTime = date.toLocaleTimeString('da-DK', {
-        hour: '2-digit',
-        minute: '2-digit'
-    });
-
-    return currentTime;
-}
