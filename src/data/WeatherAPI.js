f369bd83d659daab0177e61a598105c43a3093a9
both-both <claraboth@gmail.com>
2026-03-18 09:37:19 +0100

    recreated old weahter fetch

--- src/data/WeatherAPI.js
+++ src/data/WeatherAPI.js
@@ -1,25 +1,9 @@
-const weatherURL =
-  "https://api.openweathermap.org/data/2.5/weather?q=Aalborg&appid=4d58d6f0a435bf7c5a52e2030f17682d&units=metric";
-
+const vejrURL = "https://api.openweathermap.org/data/2.5/weather?q=Aalborg&appid=4d58d6f0a435bf7c5a52e2030f17682d&units=metric"
 export async function fetchWeather() {
-  try {
-    const weatherResponse = await fetch(weatherURL);
-    if (!weatherResponse.ok)
-      throw new Error(
-        `Fetching failed: ${weatherResponse.status} ${weatherResponse.statusText}`,
-      );
+    const result = await fetch(vejrURL)
+    if (!result.ok) throw new Error(`Fetching failed: ${result.status} ${result.statusText}`);
+    const data = result.json()
 
-    const contentType = weatherResponse.headers.get("content-type") || "";
-    if (!contentType.includes("application/json")) {
-      throw new Error(
-        `Weather API returned non-JSON content (${contentType || "unknown"})`,
-      );
-    }
 
-    const WeatherData = await weatherResponse.json();
-    return WeatherData;
-  } catch (error) {
-    console.error("Error fetching weather:", error);
-    throw error;
-  }
+    return data
 }
