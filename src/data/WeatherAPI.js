e9e5f371c6e67475e320b70123382482bd3d01a9
both-both <claraboth@gmail.com>
2026-03-18 12:35:55 +0100

    all data is set up in the weather and clock module

--- src/data/WeatherAPI.js
+++ src/data/WeatherAPI.js
@@ -1,9 +1,9 @@
 const vejrURL = "https://api.openweathermap.org/data/2.5/weather?q=Aalborg&appid=4d58d6f0a435bf7c5a52e2030f17682d&units=metric"
-export async function fetchWeather() {
-    const result = await fetch(vejrURL)
-    if (!result.ok) throw new Error(`Fetching failed: ${result.status} ${result.statusText}`);
-    const data = result.json()
 
+export async function fetchWeather() {
+    const result = await fetch(vejrURL);
+    if (!result.ok) throw new Error(`Fetching failed: ${result.status} ${result.statusText}`);
+    const data = await result.json()
 
     return data
 }
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { DRNewsModule } from "./modules/DRNews.js";
 import { MenuModule } from "./modules/Menu.js";
 import { RejseplanenModule } from "./modules/Rejseplanen.js";
 import { ActivitiesModule } from "./modules/Activities.js";
+import { weatherAndClockModule } from "./modules/WeatherAndClock.js";
 
 const app = get("#app");
 
@@ -13,4 +14,5 @@ const app = get("#app");
   // set(await MenuModule(), app);
   set(await RejseplanenModule(), app);
   set(await ActivitiesModule(), app);
+  set(await weatherAndClockModule(), app);
 })();
