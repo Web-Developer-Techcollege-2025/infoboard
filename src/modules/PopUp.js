39167e221b0041b988b6a6829b4d336f9517d740
Jens L. Hammer <jens@lindberghammer.dk>
2026-04-22 13:27:33 +0200

    fix: popup timer cleanup while keeping module disabled

--- src/modules/PopUp.js
+++ src/modules/PopUp.js
@@ -7,13 +7,25 @@ export function popup() {
     "pop-up fixed inset-0 hidden items-center justify-center",
   );
 
-  setInterval(() => {
+  let showIntervalId = null;
+  let hideTimeoutId = null;
+
+  showIntervalId = setInterval(() => {
     modal.classList.remove("hidden");
     modal.classList.add("flex");
-    setTimeout(() => {
+    hideTimeoutId = setTimeout(() => {
       modal.classList.add("hidden");
     }, 25_000); /* Shows for 25 seconds */
   }, 420_000); /* Every 7 minutes */
+
+  modal.destroyModule = () => {
+    if (showIntervalId) {
+      clearInterval(showIntervalId);
+    }
+    if (hideTimeoutId) {
+      clearTimeout(hideTimeoutId);
+    }
+  };
 
   modal.append(PopUpContent());
 
